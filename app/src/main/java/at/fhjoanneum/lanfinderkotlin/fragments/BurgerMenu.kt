ce28073c64a8bccb0d98a5e0560b9606867cdf3a
Bence Gacs <99565818+billgec@users.noreply.github.com>
2023-05-29 20:55:00 +0200

    Fix error burgermenu

--- app/src/main/java/at/fhjoanneum/lanfinderkotlin/fragments/BurgerMenu.kt
+++ app/src/main/java/at/fhjoanneum/lanfinderkotlin/fragments/BurgerMenu.kt
@@ -1,6 +1,6 @@
 package at.fhjoanneum.lanfinderkotlin.fragments
 
-import android.app.Fragment
+import androidx.fragment.app.Fragment
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
