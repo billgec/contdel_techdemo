2d53f504a0dd6fb29f0e486e05b3d7bec2c48270
Lamprecht Benjamin <benjamin.lamprecht1@edu.fh-joanneum.at>
2023-06-15 08:41:40 +0200

    added comment

--- app/src/main/java/at/fhjoanneum/lanfinderkotlin/restapi/services/UserController.kt
+++ app/src/main/java/at/fhjoanneum/lanfinderkotlin/restapi/services/UserController.kt
@@ -2,7 +2,6 @@ package at.fhjoanneum.lanfinderkotlin.restapi.services
 
 import android.content.ContentValues.TAG
 import android.util.Log
-import at.fhjoanneum.lanfinderkotlin.restapi.dataAccess.Firebase
 import at.fhjoanneum.lanfinderkotlin.restapi.models.AccessUser
 import at.fhjoanneum.lanfinderkotlin.restapi.models.User
 
@@ -13,12 +12,12 @@ object UserController {
     var currentUser: User? = User("isi1903", "juergen.guoekdemir@gmail.com")
 
     // Retrieve a user by ID
-    val userId = "3dvGtdSd1e48UqvgGrdo"
+    val userId = "3dvGtdSd1e48UqvgGrdo" // id in cloud firestore
 
     init {
         accessUser.getUser(userId) { user ->
             if (user != null) {
-                currentUser = user
+                currentUser = user // current user assigned here
                 Log.d(TAG, "Retrieved user: ${user.username}, ${user.email}")
             } else {
                 Log.d(TAG, "User not found")
